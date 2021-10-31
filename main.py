8d6d65cd03e73503b3b84d6c6a0cb9533f4e6783
N4Rk0t1k <leonidpashtet@gmail.com>
2021-10-31 21:58:11 +0800

    Added settings prototype

--- main.py
+++ main.py
@@ -17,12 +17,21 @@ class MainWindow(QMainWindow):
     def load_settings(self):
         uic.loadUi("settings.ui", self)
         self.main_button.clicked.connect(self.load_main)
-        self.autorun_on.clicked.connect(Settings.autorun)
+        self.autorun_on.clicked.connect(Settings.autorun_on)
 
 
 class Settings:
-    def autorun(self):
-        print("Bebra")
+    def autorun_on(self):
+        pass
+
+    def autorun_off(self):
+        pass
+
+    def theme_light(self):
+        pass
+
+    def theme_dark(self):
+        pass
 
 
 

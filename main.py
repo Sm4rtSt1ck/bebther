f3d3ed9cd5091787d8309ec3f553764b72424e2e
N4Rk0t1k <leonidpashtet@gmail.com>
2021-10-31 21:42:36 +0800

    Added settings window

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import sys
+from typing import MutableMapping
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
-class MyWidget(QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('test.ui', self)
+        self.load_main()
+    
+    def load_main(self):
+        uic.loadUi("main.ui", self)
+        self.setting_button.clicked.connect(self.load_settings)
+    
+    def load_settings(self):
+        uic.loadUi("settings.ui", self)
+        self.main_button.clicked.connect(self.load_main)
+        self.autorun_on.clicked.connect(Settings.autorun)
 
 
-if __name__ == '__main__':
+class Settings:
+    def autorun(self):
+        print("Bebra")
+
+
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ex = MyWidget()
-    ex.show()
+    MainWindow = MainWindow()
+    MainWindow.show()
     sys.exit(app.exec_())

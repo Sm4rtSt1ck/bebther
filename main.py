dd05898958b4e41f636a5e44b4e31bcd7278e786
Vsevolod Levitan <levvsev@gmail.com>
2021-11-01 15:07:48 +0800

    Minor bug fixes

--- main.py
+++ main.py
@@ -17,19 +17,18 @@ parsers = list()
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.updateData()
+        self.load_main()
         self.fetchParsers()
         global parsers
         global currentParser
         if len(parsers) > 0:
             currentParser = parsers[0]
-        self.setting_button_3.clicked.connect(self.toggleParser)
-        self.toggleParser()
-        self.updateData()
-        self.load_main()
 
     def load_main(self):
         uic.loadUi("main.ui", self)
         self.setting_button.clicked.connect(self.load_settings)
+        self.setting_button_3.clicked.connect(self.toggleParser)
 
     def load_settings(self):
         uic.loadUi("settings.ui", self)
